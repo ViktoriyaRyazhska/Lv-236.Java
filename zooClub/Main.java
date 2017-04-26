9018b86559af7880630cf7731c16aeecb548929a
bleac <marianpulup@gmail.com>
2017-04-26 23:29:09 +0300

    javadoc version 1.1

--- zooClub/Main.java
+++ zooClub/Main.java
@@ -1,36 +1,23 @@
-
-//Домашня робота : Створити клас Зооклуб , як поля прописати в ньому List, map, Set,iterator.
-
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
-
-//(          List<Pet> list;
-//Map<Person, List<Pet>> map;
-//Set<Entry<Person, List<Pet>>> set;
-//Iterator<Entry<Person, List<Pet>>> iter;)
-// 
-//Провести ініціалізацію даних полів в конструкторі класу Зооклуб.
-//list = new ArrayList<>(); map = new HashMap<>(); set = map.entrySet();
-//iter = set.iterator();
-//Реалізувати консольне меню, таким чином щоб можна було:
-//·      Додати учасника клубу 
-//·      Додати тваринку до учасника клубу 
-//·      Видалити тваринку з учасника клубу 
-//·      Видалити учасника клубу 
-//·      Видалити конкретну тваринку зі всіх власників 
-//·      Вивести на екран зооклуб 
-//·      Вийти з програми 
-//Використати для побудови меню switch.
-
+/**
+ * This class is Main
+ *
+ * @author Marta.
+ * @version 1.1.
+ * @since 24.04.2017.
+ */
 public class Main {
 
-	public static void main(String[] args) throws IOException  {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) throws IOException {
 
-		Methods.begin();
+        /**
+         * This method start ZooClub Application.
+         *
+         * @param Nothing.
+         * @return Console with application.
+         */
+        Methods.begin();
 
-	}
+    }
 
 }
--- zooClub/Person.java
+++ zooClub/Person.java
@@ -1,102 +1,175 @@
-
-
 import java.io.Serializable;
 
-public class Person implements Comparable<Person>,Serializable {
+/**
+ * This class is Entity Person, which
+ * implements Comparable<> for sort
+ * person and Serializable for record,
+ * read object Person from file.
+ *
+ * @author Marta.
+ * @version 1.1.
+ * @since 24.04.2017.
+ */
+public class Person implements Comparable<Person>, Serializable {
 
-	private String firstName;
-	private String lastName;
-	private int age;
+    /**
+     * String field firstName.
+     */
+    private String firstName;
 
-	public Person() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * String field lastName.
+     */
+    private String lastName;
 
-	public Person(String firstName, String lastName, int age) {
-		super();
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.age = age;
-	}
+    /**
+     * int field age.
+     */
+    private int age;
 
-	public String getFirstName() {
-		return firstName;
-	}
+    /**
+     * Default constructor class Person.
+     */
+    public Person() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+    /**
+     * Constructor with parameters.
+     * <code>String firstName, String lastName, int age</code>
+     */
+    public Person(String firstName, String lastName, int age) {
+        super();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
 
-	public String getLastName() {
-		return lastName;
-	}
+    /**
+     * This method get firstName.
+     *
+     * @param Nothing.
+     * @return String firstName person.
+     */
+    public String getFirstName() {
+        return firstName;
+    }
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+    /**
+     * This method set firstName Person.
+     *
+     * @param String This for setFirstName.
+     * @return Nothing.
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-	public int getAge() {
-		return age;
-	}
+    /**
+     * This method get lastName Person.
+     *
+     * @param Nothing.
+     * @return String lastName person.
+     */
+    public String getLastName() {
+        return lastName;
+    }
 
-	public void setAge(int age) {
-		this.age = age;
-	}
+    /**
+     * This method set lastName Person.
+     *
+     * @param String This for setLastName.
+     * @return Nothing.
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-	@Override
-	public String toString() {
-		return "Person [firstName=" + firstName + ", lastName=" + lastName
-				+ ", age=" + age + "]";
-	}
+    /**
+     * This method get age Person.
+     *
+     * @param Nothing.
+     * @return int age person.
+     */
+    public int getAge() {
+        return age;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + age;
-		result = prime * result
-				+ ((firstName == null) ? 0 : firstName.hashCode());
-		result = prime * result
-				+ ((lastName == null) ? 0 : lastName.hashCode());
-		return result;
-	}
+    /**
+     * This method set age Person.
+     *
+     * @param int This for setAge.
+     * @return Nothing.
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Person other = (Person) obj;
-		if (age != other.age)
-			return false;
-		if (firstName == null) {
-			if (other.firstName != null)
-				return false;
-		} else if (!firstName.equals(other.firstName))
-			return false;
-		if (lastName == null) {
-			if (other.lastName != null)
-				return false;
-		} else if (!lastName.equals(other.lastName))
-			return false;
-		return true;
-	}
+    @Override
+    public String toString() {
+        return "Person [firstName=" + firstName + ", lastName=" + lastName
+                + ", age=" + age + "]";
+    }
 
-	@Override
-	public int compareTo(Person o) {
-		int compareLastName = this.lastName.compareTo(o.lastName);
-		int compareFirstName = this.firstName.compareTo(o.firstName);
-		int compareAge = this.age - o.age;
-		if (compareLastName != 0) {
-			return compareLastName;
-		}
-		if (compareFirstName != 0) {
-			return compareFirstName;
-		}
-		return compareAge;
-	}
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + age;
+        result = prime * result
+                + ((firstName == null) ? 0 : firstName.hashCode());
+        result = prime * result
+                + ((lastName == null) ? 0 : lastName.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Person other = (Person) obj;
+        if (age != other.age)
+            return false;
+        if (firstName == null) {
+            if (other.firstName != null)
+                return false;
+        } else if (!firstName.equals(other.firstName))
+            return false;
+        if (lastName == null) {
+            if (other.lastName != null)
+                return false;
+        } else if (!lastName.equals(other.lastName))
+            return false;
+        return true;
+    }
+
+    /**
+     * This method is sorting Person by:
+     * lastname - <code>(compareLastName != 0)==true</code>;
+     * <code>else if</code>
+     * firsname - <code>(compareFirstName != 0)==true</code>;
+     * <code>else</code>
+     * age - <code>return int compareAge = this.age - o.age</code>.
+     *
+     * @param object Person.
+     * @return int.
+     */
+    @Override
+    public int compareTo(Person o) {
+        int compareLastName = this.lastName.compareTo(o.lastName);
+        int compareFirstName = this.firstName.compareTo(o.firstName);
+        int compareAge = this.age - o.age;
+        if (compareLastName != 0) {
+            return compareLastName;
+        }
+        if (compareFirstName != 0) {
+            return compareFirstName;
+        }
+        return compareAge;
+    }
 }
