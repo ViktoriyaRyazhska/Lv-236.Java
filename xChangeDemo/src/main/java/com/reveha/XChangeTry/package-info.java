489fa2f926ac2d72c016094cade1aa510a2a14de
Kolja <myndykmykola@gmail.com>
2017-05-04 19:15:13 +0300

    some

--- xChangeDemo/src/main/java/com/reveha/XChangeTry/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Main package to work with data and for its representing.
- * <p>
- * Contains: GUI, Main class where this program starts,
- * class that make all calculations.
- *
- * @author Orest Reveha
- */
-package com.reveha.XChangeTry;
